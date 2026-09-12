61c9e8be5f117ef7be37c626fc0db88109a72b15
naguluri-Sony <nagulurisony@gmail.com>
2026-09-12 19:02:28 +0530

    Add tic tac toe game

---
+++ tic_tac_toe.py
@@ -0,0 +1,56 @@
+board = [" ", " ", " ",
+         " ", " ", " ",
+         " ", " ", " "]
+
+def display_board():
+    print()
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("--+---+--")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("--+---+--")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print()
+
+def check_winner(player):
+    winning_combinations = [
+        (0, 1, 2),
+        (3, 4, 5),
+        (6, 7, 8),
+        (0, 3, 6),
+        (1, 4, 7),
+        (2, 5, 8),
+        (0, 4, 8),
+        (2, 4, 6)
+    ]
+
+    for a, b, c in winning_combinations:
+        if board[a] == board[b] == board[c] == player:
+            return True
+
+    return False
+
+def play_game():
+    player = "X"
+
+    for turn in range(9):
+        display_board()
+
+        position = int(input(f"Player {player}, choose a position (1-9): ")) - 1
+
+        if position < 0 or position > 8 or board[position] != " ":
+            print("Invalid position. Try again.")
+            continue
+
+        board[position] = player
+
+        if check_winner(player):
+            display_board()
+            print(f"Player {player} wins!")
+            return
+
+        player = "O" if player == "X" else "X"
+
+    display_board()
+    print("It's a draw!")
+
+play_game()   
